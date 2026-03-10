48eb4ab34baf828a9287e2aae0f40bd33de62821
pawlopawlo123 <rojewskipawel123@gmail.com>
2026-03-10 20:45:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener("fetch", function() {});
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
