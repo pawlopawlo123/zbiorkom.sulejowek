d15c88fa4066244f284e251e5892c240ad2e34c5
pawlopawlo123 <rojewskipawel123@gmail.com>
2026-03-23 18:19:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -246,9 +246,17 @@ function renderStop(stop,times){
     <div class="departure" style="background:${gradient(data.color)}">
 
       <div class="lineBox" style="background:${data.color}">
-        <div>🚌</div>
-        <span>${line}</span>
-      </div>
+  
+  <svg class="busIcon" viewBox="0 0 24 24" fill="currentColor">
+    <rect x="3" y="4" width="18" height="12" rx="3"></rect>
+    <rect x="5" y="6" width="14" height="5" rx="1"></rect>
+    <circle cx="7" cy="18" r="2"></circle>
+    <circle cx="17" cy="18" r="2"></circle>
+  </svg>
+
+  <span class="lineNumber">${line}</span>
+
+</div>
 
       <div class="middle">
         <div class="direction">${data.direction}</div>
